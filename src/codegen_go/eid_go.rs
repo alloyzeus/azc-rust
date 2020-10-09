d9bf35c1110dd2948545a05b4fd7c9768d82d71f
Fahrezal Effendi <exavolt@gmail.com>
2020-10-09 21:30:55 +0000

    wip: refactor id bitfields

--- src/codegen_go/eid_go.rs
+++ src/codegen_go/eid_go.rs
@@ -8,6 +8,7 @@ use azml::azml::eid;
 pub struct IntegerIdContext {
     primitive_size: i8,
     type_name: String,
+    bitfield: IntegerIdBitfieldContext,
     flags: Vec<IntegerIdBitFlagContext>,
 }
 
@@ -16,6 +17,8 @@ impl From<&eid::IntegerId> for IntegerIdContext {
         IntegerIdContext {
             primitive_size: x.primitive_size(),
             type_name: format!("int{}", x.primitive_size()),
+            // Minus 2: one for making it zero-based index, one for skipping the first bit (sign-bit)
+            bitfield: IntegerIdBitfieldContext::from(&x.bitfield, x.primitive_size() - 2),
             flags: x.flags.iter().map(|x| x.into()).collect(),
         }
     }
@@ -55,6 +58,104 @@ pub struct IntegerIdBitFlagBitContext {
 impl From<&eid::IntegerIdBitFlagBit> for IntegerIdBitFlagBitContext {
     fn from(x: &eid::IntegerIdBitFlagBit) -> IntegerIdBitFlagBitContext {
         IntegerIdBitFlagBitContext {
+            index: x.index,
+            set: x.set,
+        }
+    }
+}
+
+//endregion
+
+//region IntegerIdBitfieldContext
+
+#[derive(Clone, Gtmpl)]
+pub struct IntegerIdBitfieldContext {
+    pub sub_fields: Vec<IntegerIdBitfieldSubFieldContext>,
+}
+
+impl IntegerIdBitfieldContext {
+    fn from(x: &eid::IntegerIdBitfield, index_offset: i8) -> IntegerIdBitfieldContext {
+        IntegerIdBitfieldContext {
+            sub_fields: x
+                .sub_fields
+                .iter()
+                .map(|sub_field| IntegerIdBitfieldSubFieldContext::from(sub_field, index_offset))
+                .collect(),
+        }
+    }
+}
+
+impl From<&eid::IntegerIdBitfield> for IntegerIdBitfieldContext {
+    fn from(x: &eid::IntegerIdBitfield) -> IntegerIdBitfieldContext {
+        IntegerIdBitfieldContext {
+            sub_fields: x.sub_fields.iter().map(|x| x.into()).collect(),
+        }
+    }
+}
+
+//endregion
+
+//region IntegerIdBitfieldSubFieldContext
+
+#[derive(Clone, Gtmpl)]
+pub struct IntegerIdBitfieldSubFieldContext {
+    identifier: String,
+    doc_lines: Vec<String>,
+    bits: Vec<IntegerIdBitfieldSubFieldBitContext>,
+}
+
+impl IntegerIdBitfieldSubFieldContext {
+    fn from(
+        x: &eid::IntegerIdBitfieldSubField,
+        index_offset: i8,
+    ) -> IntegerIdBitfieldSubFieldContext {
+        IntegerIdBitfieldSubFieldContext {
+            identifier: x.identifier.to_owned(),
+            doc_lines: x.documentation.lines().map(|x| x.to_owned()).collect(),
+            bits: x
+                .bits
+                .iter()
+                .map(|bit| IntegerIdBitfieldSubFieldBitContext::from(bit, index_offset))
+                .collect(),
+        }
+    }
+}
+
+impl From<&eid::IntegerIdBitfieldSubField> for IntegerIdBitfieldSubFieldContext {
+    fn from(x: &eid::IntegerIdBitfieldSubField) -> IntegerIdBitfieldSubFieldContext {
+        IntegerIdBitfieldSubFieldContext {
+            identifier: x.identifier.to_owned(),
+            doc_lines: x.documentation.lines().map(|x| x.to_owned()).collect(),
+            bits: x.bits.iter().map(|x| x.into()).collect(),
+        }
+    }
+}
+
+//endregion
+
+//region IntegerIdBitFieldSubFieldBitContext
+
+#[derive(Clone, Gtmpl)]
+pub struct IntegerIdBitfieldSubFieldBitContext {
+    pub index: i8,
+    pub set: bool,
+}
+
+impl IntegerIdBitfieldSubFieldBitContext {
+    fn from(
+        x: &eid::IntegerIdBitfieldSubFieldBit,
+        offset: i8,
+    ) -> IntegerIdBitfieldSubFieldBitContext {
+        IntegerIdBitfieldSubFieldBitContext {
+            index: offset - x.index,
+            set: x.set,
+        }
+    }
+}
+
+impl From<&eid::IntegerIdBitfieldSubFieldBit> for IntegerIdBitfieldSubFieldBitContext {
+    fn from(x: &eid::IntegerIdBitfieldSubFieldBit) -> IntegerIdBitfieldSubFieldBitContext {
+        IntegerIdBitfieldSubFieldBitContext {
             index: x.index,
             set: x.set,
         }
